8f3edfe2bd123d2c672f40d073b967f11ca03fdf
Giorno-Giovanna-Dio <dcetpn@gmail.com>
2025-11-16 09:37:43 +0800

    Initial Commit

---
+++ ConversionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConversionApp
+//
+//  Created by David Chung on 2025/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ConversionApp/ConversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionAppApp.swift
+//  ConversionApp
+//
+//  Created by David Chung on 2025/11/16.
+//
+
+import SwiftUI
+
+@main
+struct ConversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

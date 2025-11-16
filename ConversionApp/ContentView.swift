84c5eca425a10a20453b3ce840cd6f284b021db3
Giorno-Giovanna-Dio <dcetpn@gmail.com>
2025-11-16 10:09:56 +0800

    first commit

--- ConversionApp/ContentView.swift
+++ ConversionApp/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var lbWeight: Double = 0.0
+
+    // Computed property that derives kilograms from the current pounds
+    var kilograms: Double {
+        lbWeight / 2.20462
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                // input
+                Section("convert lbs to Kg") {
+                    TextField("Amount", value: $lbWeight, format: .number)
+                        .keyboardType(.decimalPad)
+                }
+                // output
+                Section("Weight in Kg") {
+                    Text(kilograms, format: .number)
+                        .font(.largeTitle)
+                        .foregroundColor(.blue)
+                        .padding()
+                }
+            }
+            .navigationTitle("ezConvert")
+            
         }
-        .padding()
     }
 }
 
